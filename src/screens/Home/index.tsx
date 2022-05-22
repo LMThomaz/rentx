0dc2557b1a39c6eb66d3ceb41bd9e09e9b8da378
Leonardo Thomaz <leonardo.thomaz@hotmail.com>
2022-05-21 21:41:01 -0300

    Component Header styled

--- src/screens/Home/index.tsx
+++ src/screens/Home/index.tsx
@@ -1,9 +1,22 @@
-import { Container, Title } from './styles';
+import { StatusBar } from 'react-native';
+import { Container, Header, HeaderContent, TotalCars } from './styles';
+import Logo from '../../assets/logo.svg';
+import { RFValue } from 'react-native-responsive-fontsize';
 
 export function Home() {
   return (
     <Container>
-      <Title>Hello World!</Title>
+      <StatusBar
+        translucent
+        barStyle='light-content'
+        backgroundColor='transparent'
+      />
+      <Header>
+        <HeaderContent>
+          <Logo width={RFValue(108)} height={RFValue(12)} />
+          <TotalCars>Total de 12 carros</TotalCars>
+        </HeaderContent>
+      </Header>
     </Container>
   );
 }
--- src/screens/Home/styles.tsx
+++ src/screens/Home/styles.tsx
@@ -1,13 +1,26 @@
+import { RFValue } from 'react-native-responsive-fontsize';
 import styled from 'styled-components/native';
 
 export const Container = styled.View`
   flex: 1;
-  justify-content: center;
-  align-items: center;
   background: ${({ theme }) => theme.colors.background_primary};
 `;
 
-export const Title = styled.Text`
-  font-size: 30px;
-  font-family: ${({ theme }) => theme.fonts.secondary_600};
+export const Header = styled.View`
+  width: 100%;
+  height: 113px;
+  background: ${({ theme }) => theme.colors.header};
+  justify-content: flex-end;
+`;
+
+export const HeaderContent = styled.View`
+  justify-content: space-between;
+  flex-direction: row;
+  padding: 32px 24px;
+`;
+
+export const TotalCars = styled.Text`
+  font-size: ${RFValue(15)}px;
+  font-family: ${({ theme }) => theme.fonts.primary_400};
+  color: ${({ theme }) => theme.colors.text};
 `;
