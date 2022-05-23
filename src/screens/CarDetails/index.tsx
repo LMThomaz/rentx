d93f89067fd6ecf82e61c850497579b70691021f
Leonardo Thomaz <leonardo.thomaz@hotmail.com>
2022-05-23 00:36:09 -0300

    Details and About in screen CarDetails

--- src/screens/CarDetails/index.tsx
+++ src/screens/CarDetails/index.tsx
@@ -1,6 +1,19 @@
 import { BackButton } from '../../components/BackButton';
 import { ImageSlider } from '../../components/ImageSlider';
-import { Container, Header, CarImages } from './styles';
+import {
+  Container,
+  Header,
+  CarImages,
+  Content,
+  Details,
+  Description,
+  Brand,
+  Name,
+  Rent,
+  Period,
+  Price,
+  About,
+} from './styles';
 
 export function CarDetails() {
   return (
@@ -15,6 +28,24 @@ export function CarDetails() {
           ]}
         />
       </CarImages>
+      <Content>
+        <Details>
+          <Description>
+            <Brand>Lamborghini</Brand>
+            <Name>Huracan</Name>
+          </Description>
+          <Rent>
+            <Period>Ao dia</Period>
+            <Price>R$ 580</Price>
+          </Rent>
+        </Details>
+        <About>
+          Lorem ipsum dolor sit amet consectetur adipisicing elit. Dolore
+          recusandae quam incidunt rem omnis, adipisci vel beatae, eum, laborum
+          nihil saepe quibusdam! Laborum veritatis nobis quae debitis animi
+          cupiditate autem?
+        </About>
+      </Content>
     </Container>
   );
 }
