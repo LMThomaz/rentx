70dc8e16ffdd5e2b01f14edaeb0af6e63f87c3b6
Leonardo Thomaz <leonardo.thomaz@hotmail.com>
2022-06-01 09:11:50 -0300

    Pass car data by params screen Home to CarDetails

--- src/screens/CarDetails/index.tsx
+++ src/screens/CarDetails/index.tsx
@@ -24,16 +24,26 @@ import gasolineSvg from '../../assets/gasoline.svg';
 import exchangeSvg from '../../assets/exchange.svg';
 import peopleSvg from '../../assets/people.svg';
 import { Button } from '../../components/Button';
-import { useNavigation } from '@react-navigation/native';
+import { useNavigation, useRoute } from '@react-navigation/native';
 import { StackRoutesName } from '../../routes/stack.routes';
 import { NativeStackNavigationProp } from '@react-navigation/native-stack';
 import { StatusBar } from 'react-native';
+import { CarTDO } from '../../dtos/CarTDO';
+
+interface Params {
+  car: CarTDO;
+}
 
 export function CarDetails() {
   const navigation =
     useNavigation<NativeStackNavigationProp<StackRoutesName>>();
+  const route = useRoute();
+  const { car } = route.params as Params;
   function handleConfirmRental() {
     navigation.navigate('Scheduling');
+  }
+  function handleBack() {
+    navigation.goBack();
   }
 
   return (
@@ -44,40 +54,32 @@ export function CarDetails() {
         backgroundColor='transparent'
       />
       <Header>
-        <BackButton onPress={() => {}} />
+        <BackButton onPress={handleBack} />
       </Header>
       <CarImages>
-        <ImageSlider
-          imagesUrl={[
-            'https://png.monster/wp-content/uploads/2020/11/2018-audi-rs5-4wd-coupe-angular-front-5039562b.png',
-          ]}
-        />
+        <ImageSlider imagesUrl={car.photos} />
       </CarImages>
       <Content>
         <Details>
           <Description>
-            <Brand>Lamborghini</Brand>
-            <Name>Huracan</Name>
+            <Brand>{car.brand}</Brand>
+            <Name>{car.name}</Name>
           </Description>
           <Rent>
-            <Period>Ao dia</Period>
-            <Price>R$ 580</Price>
+            <Period>{car.rent.period}</Period>
+            <Price>R$ {car.rent.price}</Price>
           </Rent>
         </Details>
         <Accessories>
-          <Accessory name='380Km/h' icon={speedSvg} />
-          <Accessory name='3.2s' icon={accelerationSvg} />
-          <Accessory name='800 HP' icon={forceSvg} />
-          <Accessory name='Gasolina' icon={gasolineSvg} />
-          <Accessory name='Auto' icon={exchangeSvg} />
-          <Accessory name='2 Pessoas' icon={peopleSvg} />
+          {car.accessories.map((accessory) => (
+            <Accessory
+              key={accessory.type}
+              name={accessory.name}
+              icon={speedSvg}
+            />
+          ))}
         </Accessories>
-        <About>
-          Lorem ipsum dolor sit amet consectetur adipisicing elit. Dolore
-          recusandae quam incidunt rem omnis, adipisci vel beatae, eum, laborum
-          nihil saepe quibusdam! Laborum veritatis nobis quae debitis animi
-          cupiditate autem?
-        </About>
+        <About>{car.about}</About>
       </Content>
       <Footer>
         <Button
--- src/screens/Home/index.tsx
+++ src/screens/Home/index.tsx
@@ -1,23 +1,23 @@
 import { useNavigation } from '@react-navigation/native';
 import { NativeStackNavigationProp } from '@react-navigation/native-stack';
-import { Alert, StatusBar } from 'react-native';
+import { useEffect, useState } from 'react';
+import { StatusBar } from 'react-native';
 import { RFValue } from 'react-native-responsive-fontsize';
 import Logo from '../../assets/logo.svg';
 import { Car } from '../../components/Car';
 import { Load } from '../../components/Load';
-import { StackRoutesName } from '../../routes/stack.routes';
-import { CarList, Container, Header, HeaderContent, TotalCars } from './styles';
-import { api } from '../../services/api';
-import { useEffect, useState } from 'react';
 import { CarTDO } from '../../dtos/CarTDO';
+import { StackRoutesName } from '../../routes/stack.routes';
+import { api } from '../../services/api';
+import { CarList, Container, Header, HeaderContent, TotalCars } from './styles';
 
 export function Home() {
   const [cars, setCars] = useState<CarTDO[]>([]);
   const [loading, setLoading] = useState(true);
   const navigation =
     useNavigation<NativeStackNavigationProp<StackRoutesName>>();
-  function handleCarDetails() {
-    navigation.navigate('CarDetails');
+  function handleCarDetails(car: CarTDO) {
+    navigation.navigate('CarDetails', { car });
   }
   useEffect(() => {
     async function fetchCars() {
@@ -54,7 +54,7 @@ export function Home() {
           data={cars}
           keyExtractor={(item) => item.id}
           renderItem={({ item }) => (
-            <Car data={item} onPress={handleCarDetails} />
+            <Car data={item} onPress={() => handleCarDetails(item)} />
           )}
         />
       )}
