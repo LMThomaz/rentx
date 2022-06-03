9f2a5556c9a0f28862c4604e9973947be7633302
Leonardo Thomaz <leonardo.thomaz@hotmail.com>
2022-06-03 10:24:19 -0300

    Showing cars sheduling

--- src/screens/MyCars/index.tsx
+++ src/screens/MyCars/index.tsx
@@ -1,23 +1,81 @@
+import { useNavigation } from '@react-navigation/native';
+import { NativeStackNavigationProp } from '@react-navigation/native-stack';
 import { useEffect, useState } from 'react';
-import { Container } from './styles';
+import { FlatList, StatusBar } from 'react-native';
+import { useTheme } from 'styled-components';
+import { BackButton } from '../../components/BackButton';
+import { Car } from '../../components/Car';
 import { CarTDO } from '../../dtos/CarTDO';
+import { StackRoutesName } from '../../routes/stack.routes';
 import { api } from '../../services/api';
+import {
+  Container,
+  Header,
+  Title,
+  SubTitle,
+  Content,
+  Appointments,
+  AppointmentsTitle,
+  AppointmentsQuantity,
+} from './styles';
+
+interface CarProps {
+  id: string;
+  user_id: string;
+  car: CarTDO;
+}
 
 export function MyCars() {
-  const [cars, setCars] = useState<CarTDO[]>([]);
+  const [cars, setCars] = useState<CarProps[]>([]);
   const [loading, setLoading] = useState(true);
+  const theme = useTheme();
+  const navigation =
+    useNavigation<NativeStackNavigationProp<StackRoutesName>>();
+  function handleBack() {
+    navigation.goBack();
+  }
   useEffect(() => {
     async function fetchCars() {
       try {
         const response = await api.get('/schedules_by_user?user_id=1');
-        setCars(response.data.car);
+        setCars(response.data);
       } catch (error) {
         console.log(error);
       } finally {
         setLoading(false);
       }
     }
+    fetchCars();
   }, []);
 
-  return <Container></Container>;
+  return (
+    <Container>
+      <Header>
+        <StatusBar
+          barStyle='light-content'
+          translucent
+          backgroundColor='transparent'
+        />
+        <BackButton color={theme.colors.shape} onPress={handleBack} />
+        <Title>
+          Escolha uma{'\n'}
+          data de início e{'\n'}
+          fim do aluguel
+        </Title>
+        <SubTitle>Conforto, segurança e praticidade.</SubTitle>
+      </Header>
+      <Content>
+        <Appointments>
+          <AppointmentsTitle>Agendamentos feitos</AppointmentsTitle>
+          <AppointmentsQuantity>05</AppointmentsQuantity>
+        </Appointments>
+        <FlatList
+          data={cars}
+          keyExtractor={(item) => item.id}
+          showsVerticalScrollIndicator={false}
+          renderItem={({ item }) => <Car data={item.car} />}
+        />
+      </Content>
+    </Container>
+  );
 }
