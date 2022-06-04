9b4253f4e073d5942eed78a0714c2d71deb6ed19
Leonardo Thomaz <leonardo.thomaz@hotmail.com>
2022-06-04 19:53:39 -0300

    Transactions in animated

--- src/screens/Splash/index.tsx
+++ src/screens/Splash/index.tsx
@@ -1,19 +1,30 @@
 import { Container } from './styles';
-import { Button, StyleSheet } from 'react-native';
+import { Button, StyleSheet, Dimensions } from 'react-native';
 import Animated, {
   useSharedValue,
   useAnimatedStyle,
+  withTiming,
+  Easing,
 } from 'react-native-reanimated';
+
+const WIDTH = Dimensions.get('window').width;
 
 export function Splash() {
   const animation = useSharedValue(0);
   const animatedStyles = useAnimatedStyle(() => {
     return {
-      transform: [{ translateX: animation.value }],
+      transform: [
+        {
+          translateX: withTiming(animation.value, {
+            duration: 160,
+            easing: Easing.sin,
+          }),
+        },
+      ],
     };
   });
   function handleAnimationPosition() {
-    animation.value = Math.random() * 300;
+    animation.value = Math.random() * (WIDTH - 100);
   }
 
   return (
