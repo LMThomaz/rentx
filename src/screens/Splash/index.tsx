e371414b6255948df41153e7ecd0bef198bde4d9
Leonardo Thomaz <leonardo.thomaz@hotmail.com>
2022-06-04 19:46:28 -0300

    first step with animation

--- src/screens/Splash/index.tsx
+++ src/screens/Splash/index.tsx
@@ -1,6 +1,33 @@
-import { Text } from 'react-native';
 import { Container } from './styles';
+import { Button, StyleSheet } from 'react-native';
+import Animated, {
+  useSharedValue,
+  useAnimatedStyle,
+} from 'react-native-reanimated';
 
 export function Splash() {
-  return <Container></Container>;
+  const animation = useSharedValue(0);
+  const animatedStyles = useAnimatedStyle(() => {
+    return {
+      transform: [{ translateX: animation.value }],
+    };
+  });
+  function handleAnimationPosition() {
+    animation.value = Math.random() * 300;
+  }
+
+  return (
+    <Container>
+      <Animated.View style={[styles.box, animatedStyles]} />
+      <Button title='Mover' onPress={handleAnimationPosition} />
+    </Container>
+  );
 }
+
+const styles = StyleSheet.create({
+  box: {
+    width: 100,
+    height: 100,
+    backgroundColor: 'red',
+  },
+});
