13636c7b8609c9a1a2a466f4bdff1e45c013b7fa
Leonardo Thomaz <leonardo.thomaz@hotmail.com>
2022-06-13 20:53:03 -0300

    Accessibility in Form

--- src/screens/SignIn/index.tsx
+++ src/screens/SignIn/index.tsx
@@ -1,4 +1,9 @@
-import { StatusBar } from 'react-native';
+import {
+  KeyboardAvoidingView,
+  StatusBar,
+  TouchableWithoutFeedback,
+  Keyboard,
+} from 'react-native';
 import { Container, Footer, Form, Header, SubTitle, Title } from './styles';
 import { Button } from '../../components/Button';
 import { useTheme } from 'styled-components';
@@ -9,44 +14,48 @@ export function SignIn() {
   const theme = useTheme();
 
   return (
-    <Container>
-      <StatusBar
-        barStyle='dark-content'
-        translucent
-        backgroundColor='transparent'
-      />
-      <Header>
-        <Title>Estamos{'\n'}quase lá.</Title>
-        <SubTitle>
-          Faça seu login para começar{'\n'}uma experiência incrível.
-        </SubTitle>
-      </Header>
-      <Form>
-        <Input
-          iconName='mail'
-          placeholder='E-mail'
-          keyboardType='email-address'
-          autoCorrect={false}
-          autoCapitalize='none'
-        />
-        <PasswordInput iconName='lock' placeholder='Senha' />
-      </Form>
-      <Footer>
-        <Button
-          title='Login'
-          onPress={() => {}}
-          enabled={false}
-          loading={false}
-        />
-        <Button
-          title='Criar conta gratuita'
-          color={theme.colors.background_secondary}
-          onPress={() => {}}
-          enabled={false}
-          loading={false}
-          light
-        />
-      </Footer>
-    </Container>
+    <KeyboardAvoidingView behavior='position' enabled>
+      <TouchableWithoutFeedback onPress={Keyboard.dismiss}>
+        <Container>
+          <StatusBar
+            barStyle='dark-content'
+            translucent
+            backgroundColor='transparent'
+          />
+          <Header>
+            <Title>Estamos{'\n'}quase lá.</Title>
+            <SubTitle>
+              Faça seu login para começar{'\n'}uma experiência incrível.
+            </SubTitle>
+          </Header>
+          <Form>
+            <Input
+              iconName='mail'
+              placeholder='E-mail'
+              keyboardType='email-address'
+              autoCorrect={false}
+              autoCapitalize='none'
+            />
+            <PasswordInput iconName='lock' placeholder='Senha' />
+          </Form>
+          <Footer>
+            <Button
+              title='Login'
+              onPress={() => {}}
+              enabled={false}
+              loading={false}
+            />
+            <Button
+              title='Criar conta gratuita'
+              color={theme.colors.background_secondary}
+              onPress={() => {}}
+              enabled={false}
+              loading={false}
+              light
+            />
+          </Footer>
+        </Container>
+      </TouchableWithoutFeedback>
+    </KeyboardAvoidingView>
   );
 }
